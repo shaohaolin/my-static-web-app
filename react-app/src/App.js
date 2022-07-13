7ef53dceca0a167fd098541251c23a24296af58d
Shaohao Lin <shaohlin@microsoft.com>
2022-07-13 11:53:09 -0700

    add api

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,36 +1,43 @@
-import React, { Component, lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
-import './styles.scss';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { withRouter } from 'react-router';
 import { HeaderBar, NavBar, NotFound } from './components';
 import About from './About';
 
 const Products = withRouter(
-  lazy(() => import(/* webpackChunkName: "products" */ './products/Products'))
+  lazy(() => import(/* webpackChunkName: "products" */ './products/Products')),
 );
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <HeaderBar />
-        <div className="section columns">
-          <NavBar />
-          <main className="column">
-            <Suspense fallback={<div>Loading...</div>}>
-              <Switch>
-                <Redirect from="/" exact to="/products" />
-                <Route path="/products" component={Products} />
-                <Route path="/about" component={About} />
-                <Route exact path="**" component={NotFound} />
-              </Switch>
-            </Suspense>
-          </main>
-        </div>
+const App = () => {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await (await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
+
+  return (
+    <div>
+      <HeaderBar />
+      <div>{data}</div>
+      <div className="section columns">
+        <NavBar />
+        <main className="column">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Redirect from="/" exact to="/products" />
+              <Route path="/products" component={Products} />
+              <Route path="/about" component={About} />
+              <Route exact path="**" component={NotFound} />
+            </Switch>
+          </Suspense>
+        </main>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
